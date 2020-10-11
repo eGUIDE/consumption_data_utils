7e625b12a46a42d36e24aba15de819c5356034cd
bmuhwezi <muhwezibob@gmail.com>
2020-10-10 22:33:26 -0400

    modified:   smoothing.py

--- smoothing.py
+++ smoothing.py
@@ -18,13 +18,6 @@ def func(days,row):
 
 
 
-def days_means_medians(S):
-	m=S.sort_values().diff().dt.days
-	means=m.mean()
-	medians=m.median()
-	return (means,medians)
-
-
 def smoothing_func(df):
 	df.transaction_date=pd.to_datetime(df.transaction_date)
 	df.transaction_date=df.transaction_date.dt.date
@@ -41,13 +34,3 @@ def smoothing_func(df):
 	days=days.reset_index(drop=True)
 	return days
 
-
-DF=pd.read_pickle('/home/jtaneja/work/projects/smallcommercial/REG/REG_transaction_data.pck')
-DF=DF[DF.kWh_sold>=0]
-drp_name=['TVA tax','Regulatory_Fee','Rura_fee']
-DF=DF[~(DF.tariff_name.isin(drp_name))]
-DF.transaction_date=pd.to_datetime(DF.transaction_date)
-DF.transaction_date=DF.transaction_date.dt.date
-a=DF.groupby(['consumer_id','meter_serial_number']).kWh_sold.count()
-df1=DF[DF.consumer_id=='20121101 104121.689 0002032 0025']
-df2=DF[DF.consumer_id=='57114']
