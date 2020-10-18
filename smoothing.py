edad8248b3b7512f70718a31760b5d5c55c29aed
bmuhwezi <muhwezibob@gmail.com>
2020-10-18 00:46:03 -0400

    modified:   smoothing.py

--- smoothing.py
+++ smoothing.py
@@ -22,21 +22,22 @@ def smoothing_func(df):
 	df.transaction_date=pd.to_datetime(df.transaction_date)
 	df.transaction_date=df.transaction_date.dt.date
 	df.sort_values('transaction_date',inplace=True)
-	df['Nof_days']=-df.transaction_date.diff(periods=-1).dt.days	
-	days=df.groupby('transaction_date')['kWh_sold','Nof_days'].sum()
-	medians=df.Nof_days.median()
+	days=df.groupby('transaction_date')['kWh_sold'].sum().reset_index()
+	days['Nof_days']=-days.transaction_date.diff(periods=-1).dt.days	
+	medians=days.Nof_days.median()
 	if medians >=10:
 		min_=days.Nof_days.min()
 		Nof_days=days.Nof_days.clip(min_,medians)
 		days['kWh_per_day']=days.kWh_sold/Nof_days
 	else:
 		days['kWh_per_day']=days.kWh_sold/days.Nof_days
-	idx=days.index
+	idx=days.transaction_date
 	I=len(idx)-1
-	days=days.reset_index(drop=True).drop(columns='kWh_sold')
+	days.drop(columns=['kWh_sold','transaction_date'],inplace=True)
 	days['days']=days.apply(lambda rows: func(days,rows.name),axis=1)
 	days.days=days.days+1
 	days.days[I]=idx[I]
 	days=days.explode('days').reset_index(drop=True)
 	return days
+
 
