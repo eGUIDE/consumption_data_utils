d6018e8ddad2a3b1bafdc648e89ffc5cf205164b
bmuhwezi <muhwezibob@gmail.com>
2020-11-08 20:13:49 -0500

    modified:   smoothing.py

--- smoothing.py
+++ smoothing.py
@@ -27,10 +27,24 @@ def smoothing_func(df,frq):
 		days['Nof_days']=-days.transaction_date.diff(periods=-1).dt.days	
 		medians=days.Nof_days.median()
 		days.Nof_days=days.Nof_days.fillna(medians)
-		if medians >=frq:
+		if (medians >=frq)&(medians<100):
 			min_=days.Nof_days.min()
 			Nof_days=days.Nof_days.clip(min_,medians)
 			days['kWh_per_day']=days.kWh_sold/Nof_days
+			earliest_transaction_date = days.transaction_date.min()
+			days['days']=days.Nof_days.apply(lambda rows: np.arange(rows))
+			days_daily=days.explode('days').reset_index(drop=True)
+			days_daily=days_daily[days_daily.days<=medians]
+			days_daily.days=days_daily.index.values+1
+		elif medians>=100:
+			min_=days.Nof_days.min()
+			Nof_days=days.Nof_days.clip(min_,100)
+			days['kWh_per_day']=days.kWh_sold/Nof_days
+			earliest_transaction_date = days.transaction_date.min()
+			days['days']=days.Nof_days.apply(lambda rows: np.arange(rows))
+			days_daily=days.explode('days').reset_index(drop=True)
+			days_daily=days_daily[days_daily.days<=100]
+			days_daily.days=days_daily.index.values+1
 		else:
 			normlzd=days.kWh_sold/days.Nof_days
 			qtl=normlzd.quantile(0.1)
@@ -40,13 +54,9 @@ def smoothing_func(df,frq):
 			days['normlzd']=Nof_days
 			max_=normlzd.max()
 			days['kWh_per_day']=normlzd.clip(qtl,max_)
-		earliest_transaction_date = days.transaction_date.min()
-		days['days']=days.Nof_days.apply(lambda rows: np.arange(rows))
-		days_daily=days.explode('days').reset_index(drop=True)
-		if medians >=frq:
-			days_daily=days_daily[days_daily.days<=medians]
-			days_daily.days=days_daily.index.values+1
-		else:
+			earliest_transaction_date = days.transaction_date.min()
+			days['days']=days.Nof_days.apply(lambda rows: np.arange(rows))
+			days_daily=days.explode('days').reset_index(drop=True)
 			days_daily=days_daily[days_daily.normlzd>days_daily.days]
 			days_daily.days=days_daily.index.values+1
 			days_daily.drop(columns='normlzd',inplace=True)
